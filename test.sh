51a1b31b680039f2f969cb08f1d105a522209009
kimCoding <xowns4817@naver.com>
2021-02-15 23:16:57 +0900

    add comment

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ sessionKey=""
 findFlag=0
 closeFlag="CLOSE"
 rowNum=0
-totalNum=$(cat dialog.data | wc -l)
+totalNum=$(cat dialog.data | wc -l) # 뽑을 로그데이터 전체 행 갯수
 tailIdx=0
 
 echo "===================="
@@ -20,7 +20,7 @@ while read line
 	tailIdx=`expr ${totalNum} - ${rowNum} + 1` # tail을 어디서 부터 잡아야 할지 설정
 	while read line2
 	do
-	if [ "$closeFlag" == "$line2" ]
+	if [ "$closeFlag" == "$line2" ] # CLOSE를 발견할때까지 loop 탐색
 	then
 		result=$result'"'
 		result=$result'\n'
@@ -28,7 +28,7 @@ while read line
 	else
 		result=$result$line2'\n'
 	fi
-	done < <(tail -n -$tailIdx dialog.data)
+	done < <(tail -n -$tailIdx dialog.data) # 찾은 sessionKey부터 아래로 탐색
 done < sessionKeyList.data
 
 echo -e "$result" > test.txt
